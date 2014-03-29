ab01a650e338adb471418df4f47ae5b418ce1ed8
fool2fish <fool2fish@gmail.com>
2014-03-29 08:18:16 +0800

    Update.

--- examples/context.js
+++ examples/context.js
@@ -1,17 +1,20 @@
 module.exports = {
-  user: '>>>> user <<<<<',
-  userNotActive: '==== user not active ===',
-  name: 'fool2fish',
-  info: {
-    job: 'developer',
-    lang: 'node'
+  id: '00000001',
+  user: {
+    name: 'fool2fish',
+    github: 'https://github.com/fool2fish',
+    favorites: ['food', 'travel', 'comic', '...']
   },
-  list: ['first', 'second', 'third'],
-  method: {
-    foo: function(){return 'method return'},
-    date: function(y, m, d) {
-      return y + ' - ' + m + ' - ' + d
+  date: {
+    weather: 'rain',
+    year: 2014,
+    month: 3,
+    day: 28,
+    format: function(y, m, d) {
+      return y + ' - ' + m  + ' - ' + d
     }
   },
-  eval: '$name'
+  foo: {
+    noReturn: function(){}
+  }
 }
