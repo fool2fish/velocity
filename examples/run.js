9a36264fccf346a7d06b9ba0a9983090e1bcc647
fool2fish <fool2fish@gmail.com>
2014-04-10 17:51:55 +0800

    Add viewing dependency in example.

--- examples/run.js
+++ examples/run.js
@@ -4,17 +4,26 @@ var util = require('util')
 var Engine = require('../lib/engine')
 var parser = require('../lib/engine/velocity')
 var lexicalParser = require('../lib/engine/lex')
-var STATS = require('../lib/engine/engine-stats.js')
+var dep = require('../lib/dep')
 
 var action = 'render'
 process.argv.forEach(function(item, idx, list) {
-  if (item in {ast: true, tokens: true}) action = item
+  if (item in {ast: true, tokens: true, dep: true}) action = item
 })
 
 var file = './root1/index.vm'
 var content = fs.readFileSync(file, {encoding: 'utf8'})
 
-if (action === 'tokens') {
+var cfg = {
+  roots: ['root1', 'root2', 'root3'],
+  template: file,
+  macro: './global-macro/macro.vm'
+}
+
+if (action === 'dep') {
+  dep(cfg)
+
+} else if (action === 'tokens') {
   var tokens = lexicalParser.parse(content)
   console.log(util.inspect(tokens, {depth: null}))
 
@@ -24,14 +33,14 @@ if (action === 'tokens') {
 
 } else {
   var context = require('./context')
-  var cfg = {
-    roots: ['root1', 'root2', 'root3'],
-    template: file,
-    macro: './global-macro/macro.vm'
-  }
+
   var engine = new Engine(cfg)
   var result = engine.render(context)
-  console.log(result)
+  if (result.success) {
+    console.log(result.value)
+  } else {
+    console.log(result)
+  }
 }
 
 
