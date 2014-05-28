ae8631e4ce3e0babcec844f237bb89012d6b72ed
达峰 <dafeng.xdf@taobao.com>
2014-05-28 22:36:55 +0800

    xdf commit at 2014.05.28-22:08:29

---
+++ examples/data-dump/context.js
@@ -0,0 +1,9 @@
+module.exports = {
+  id: '000000001',
+  user: {
+    name: 'fool2fish',
+    email: 'fool2fish@gmail.com',
+    undefinedIsPreserved: undefined },
+  method: { foo: function () { return '\' \t \\' } },
+  g:'g'
+  }
