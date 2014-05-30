da04ac793fea270820d383825d01a2a48b838894
dafeng.xdf <dafeng.xdf@taobao.com>
2014-05-30 11:12:40 +0800

    add dump creator example

--- examples/data-dump/velocity-config.js
+++ examples/data-dump/velocity-config.js
@@ -1,3 +1,7 @@
+/**
+ * set dump open and the path
+ * file will be output to this path and the original context will be preserved such as the example
+ */
 module.exports = {
   root: './',
   template: './index.vm',
