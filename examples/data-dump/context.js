632e28d1d2c25f1d8222370165a3e75449ae7fbd
dafeng.xdf <dafeng.xdf@taobao.com>
2014-05-30 16:40:35 +0800

    Add dump example.

---
+++ examples/data-dump/context.js
@@ -0,0 +1,8 @@
+module.exports = {
+  user: {
+    name: 'fool2fish',
+    email: 'fool2fish@gmail.com',
+    undefinedIsPreserved: undefined },
+    method: { foo: function () { return '\' \t \\' }
+  }
+}
---
+++ examples/data-dump/velocity-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: './',
+  template: './index.vm',
+  context: './context.js',
+  dump:true,
+  output:'./data-dump.vm'
+}
