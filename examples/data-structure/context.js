37b489c31e11a3d31d9c809defecfe317e3e50f2
fool2fish <fool2fish@gmail.com>
2014-06-19 11:27:49 +0800

    Split code of function to a new example.

--- examples/data-structure/context.js
+++ examples/data-structure/context.js
@@ -3,6 +3,6 @@ module.exports = {
   user: {
     name: 'fool2fish',
     email: 'fool2fish@gmail.com',
-    undefinedIsPreserved: undefined },
-  method: { foo: function () { return '\' \t \\' } },
+    undefinedIsPreserved: undefined
   }
+}
