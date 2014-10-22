d7533ef8a132f76119757670b0bd120c71cffe5b
fool2fish <fool2fish@gmail.com>
2014-10-22 17:34:25 +0800

    Capture the error thrown by toString method.

--- examples/errors/context.js
+++ examples/errors/context.js
@@ -2,6 +2,13 @@ module.exports = {
   string: 'string',
   emptyString: '',
   method: {
-    foo: function(){ throw new Error('I just want to throw an error') }
+    foo: function() { throw new Error('I just want to throw an error') },
+    bar: function() {
+      return {
+        toString: function() {
+          throw new Error('I just want to throw an error when call toString method')
+        }
+      }
+    }
   }
 }
